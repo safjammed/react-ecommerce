e8666bb3dd20443c4aad87548ebd58c47625c80e
safjammed <safayatjamil27@gmail.com>
2020-01-27 21:41:18 +0600

    navigation added shop page, collection and collection-items created with dummy data

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component';
 
-const HomePage  = () => (
-    <div className='homepage'>
-        <Directory/>
-    </div>
-);
+const HomePage  = () => {
+    // console.log(history);
+    return (
+        <div className='homepage'>
+            <Directory/>
+        </div>
+    );
+}
 
 export default HomePage;
