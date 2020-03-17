8aa962d4a02e9ede16e2265a01e2b748947bce9e
safjammed <safayatjamil27@gmail.com>
2020-03-17 21:32:08 +0600

    Updates

---
+++ precache-manifest.6b35c6d2f38f1895daef02d5592df611.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc27574efe0394a9b671d4e4ef0741f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2428a0e82ac50aa10db5",
+    "url": "/static/css/main.7ee0e26a.chunk.css"
+  },
+  {
+    "revision": "f16e4846c7318c4de0dc",
+    "url": "/static/js/2.9df2324f.chunk.js"
+  },
+  {
+    "revision": "9cc59aa88bbb965336535eb6b360773d",
+    "url": "/static/js/2.9df2324f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2428a0e82ac50aa10db5",
+    "url": "/static/js/main.22868009.chunk.js"
+  },
+  {
+    "revision": "a18fe9d936dce06f20c5",
+    "url": "/static/js/runtime-main.6e9c3c5b.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
